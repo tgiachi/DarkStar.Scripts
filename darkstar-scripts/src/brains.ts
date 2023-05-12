a309587b37068a1a424a64a74725abd86cfa014e
tgiachi <squid@stormwind.it>
2023-05-12 20:08:55 +0200

    feat: npc search for mushrooms

--- darkstar-scripts/src/brains.ts
+++ darkstar-scripts/src/brains.ts
@@ -15,6 +15,35 @@ add_ai_brain_by_type("Human", "Mushroom_Finder", (context) => {
 	);
 	context.SendNormalMessageAsync("I'm looking for mushrooms!");
 	context.MoveRandomDirection();
+
+  if (!context.Data) {
+      context.Data = {
+        current_step: 0,
+        path: []
+      }
+  }
+  if (context.Data.path.length > 0) { 
+    if (context.Data.current_step >= context.Data.path.length) {
+      context.LogInfo("Mmmhh yummy mushroom!");
+      context.SendYellMessageAsync("I've found a yummy mushroom!");
+      context.EnqueueObjectActionInCurrentLocation();
+      context.Data.path = [];
+      context.Data.current_step = 0;
+      return context;
+    }
+    let next_step = context.Data.path[context.Data.current_step];
+    const diff_steps = context.Data.path.length - context.Data.current_step;
+    context.LogInfo(
+      "I'm {Name} and i'm moving to {NewPos} - Step remaining: {StepRemain}",
+      context.NpcEntity.Name,
+      next_step,
+      diff_steps
+    );
+    if (context.MoveToPosition(next_step)) {
+      next_step = next_step + 1;
+    }
+  }
+
 });
 
 // 	if context.Data == nil then
