10e7cb554ecb19e19faf56d826267c1d1cc2f357
Tommaso Giachi <tom@isendu.com>
2023-05-12 12:01:58 +0200

    fix: added typescript projects

---
+++ darkstar-scripts/src/index.ts
@@ -0,0 +1,8 @@
+
+
+add_city_map_generator( (context:any) => {
+
+  log("add_city_map_generator", null);
+  
+
+});
---
+++ darkstar-scripts/src/test.ts
@@ -0,0 +1,3 @@
+
+
+add_item("test", "Test", 1, "test", "test", "test", 1, 1, "test", "test", "test", "test", "test");
