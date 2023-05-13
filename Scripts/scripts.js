05ffa3ec2d129867854a67496f4c01d05ae62ea5
tgiachi <squid@stormwind.it>
2023-05-13 14:39:18 +0200

    fix: try to go to mushroom

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -20,7 +20,7 @@ add_ai_brain_by_type("Human", "Mushroom_Finder", (context) => {
     if (!context.Data) {
         context.Data = {
             current_step: 0,
-            path: []
+            path: context.CreateListOfPoints()
         };
     }
     if (context.Data.path.length > 0) {
@@ -28,51 +28,29 @@ add_ai_brain_by_type("Human", "Mushroom_Finder", (context) => {
             context.LogInfo("Mmmhh yummy mushroom!");
             context.SendYellMessageAsync("I've found a yummy mushroom!");
             context.EnqueueObjectActionInCurrentLocation();
-            context.Data.path = [];
+            context.Data.path = context.CreateListOfPoints();
             context.Data.current_step = 0;
             return context;
         }
         let next_step = context.Data.path[context.Data.current_step];
         const diff_steps = context.Data.path.length - context.Data.current_step;
         context.LogInfo("I'm {Name} and i'm moving to {NewPos} - Step remaining: {StepRemain}", context.NpcEntity.Name, next_step, diff_steps);
-        if (context.MoveToPosition(next_step)) {
-            next_step = next_step + 1;
+        if (context.MoveToPosition(next_step) === true) {
+            context.Data.current_step++;
         }
     }
+    const mushrooms = context.GetGameObjectsInRangeByName(GAMEOBJECT_PROP_MUSHROOM);
+    if (mushrooms.Count == 0) {
+        context.SendNormalMessageAsync("Sigh, no mushroom found :( !");
+        context.MoveRandomDirection();
+    }
+    else {
+        context.LogInfo("My name is {Name} and I found mushrooms! {Mushrooms}", context.NpcEntity.Name, mushrooms);
+        const random_mushroom = mushrooms.ToArray()[0];
+        context.Data.path = context.GetPathToPosition(random_mushroom.PointPosition());
+        context.Data.current_step = 0;
+    }
 });
-// 	if context.Data == nil then
-//  		-- A* data in empty
-//  		context.Data = {}
-//  		context.Data["path"] = {}
-//  		context.Data["current_step"] = 0
-// 	end
-// 	if #context.Data["path"] > 0 then
-// 		if context.Data["current_step"] >= table.getn(context.Data["path"]) then
-// 			context:LogInfo("Mmmhh yummy mushroom!")
-// 			context:SendYellMessageAsync("I've found a yummy mushroom!")
-// 			context:EnqueueObjectActionInCurrentLocation()
-// 			context.Data["path"]:Clear()
-// 			context.Data["current_step"] = 0
-// 			return context
-// 		end
-// 		local next_step = context.Data["path"][context.Data["current_step"]]
-// 		local diff_steps = context.Data["path"]:Count() - context.Data["current_step"]
-// 		context:LogInfo("I'm {Name} and i'm moving to {NewPos} - Step remaining: {StepRemain}", context.NpcEntity.Name, next_step, diff_steps)
-// 		if context:MoveToPosition(next_step) then
-// 			next_step = next_step + 1
-// 		end
-// 	end
-// 	local mushrooms = context:GetGameObjectsInRangeByName(GAMEOBJECT_PROP_MUSHROOM)
-// 	if mushrooms.Count == 0  then
-// 		context:SendNormalMessageAsync("Sigh, no mushroom found :( !")
-// 		context:MoveRandomDirection()
-// 	else
-// 		context:LogInfo("My name is {Name} and I found mushrooms! {Mushrooms}", context.NpcEntity.Name, mushrooms);
-// 		local random_mushroom = random_list(mushrooms)
-// 		context.Data["path"] = context:GetPathToPosition(random_mushroom.PointPosition())
-// 		context.Data["current_step"] = 0
-// 	end
-//  end
 on_engine_ready_event(() => {
     log("Engine is ready from JS");
 });
