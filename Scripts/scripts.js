8ab515d939264fe239cf4aceb040e362b83c0429
Tommaso Giachi <tom@isendu.com>
2023-05-15 13:19:27 +0200

    fix: removed csv

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -8,7 +8,8 @@ set_player_initial_gold(100);
 on_engine_ready_event(() => {
     log("Engine is ready from JS");
 });
-add_game_object_type("Prop_Chest", "Internal_spawner_gold", "Internal_spawner_silver");
+add_game_object_type("Prop_Chest", "Internal_spawner_gold", "Internal_spawner_silver", "Prop_Mushroom", "Internal_World_Spawner");
+add_game_object("Mushroom", "Delicius mushroom", "*Mushroom*", "Prop_Mushroom", "");
 add_game_object("Internal_spawner_gold", "Internal_spawner_gold", "3", "Internal_spawner_gold", "");
 add_game_object("Internal_spawner_silver", "Internal_spawner_silver", "4", "Internal_spawner_silver", "");
 add_game_object_action("Internal_spawner_gold", (context) => {
